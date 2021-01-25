a63a1a5507ff627a87d1fa98add8637c6870ad19
Ola Wiebe <eeken07@gmail.com>
2021-01-25 09:47:43 +0100

    add navbar

---
+++ frontend/src/Components/shared/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../../css/components/shared/Hero.scss";
+
+function Hero() {
+  return (
+    <div id="hero">
+      <h1 style={{ color: "#ffffff" }}>Best forum online!</h1>
+      {/* <form>
+        <input type="text" placeholder="Search" />
+        <button id="submitQuery">S</button>
+      </form> */}
+    </div>
+  );
+}
+
+export default Hero;
---
+++ frontend/src/Components/shared/NavBar.js
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import "../../css/components/shared/NavBar.scss";
+import { useStateValue } from "../../StateProvider";
+import { NavLink } from "react-router-dom";
+import CreatePost from "../models/CreatePost";
+
+function NavBar() {
+  const openPostModel = () => {
+    document.getElementById("register-outer").style.display = "block";
+  };
+
+  const [{ user }] = useStateValue();
+
+  const [isShowing, setIsShowing] = useState(false);
+
+  const logout = () => {
+    localStorage.setItem("loginToken", null);
+    window.location.reload();
+  };
+
+  return (
+    <>
+      <CreatePost />
+      <div id="nav-bar-conatiner">
+        <h1 id="logo">Wake Up Forum</h1>
+        <ul id="nav">
+          <NavLink
+            className="nav-item"
+            exact
+            to="/"
+            activeClassName="nav-item-active"
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            className="nav-item"
+            exact
+            to="/about"
+            activeClassName="nav-item-active"
+          >
+            About
+          </NavLink>
+          {user ? (
+            <li
+              className="nav-item nav-button"
+              id="makePostButton"
+              onClick={openPostModel}
+            >
+              Create Post
+            </li>
+          ) : (
+              <div>
+            <NavLink to="/register">
+              <div className="nav-item nav-button" id="register">
+                Register
+              </div>
+            </NavLink>
+            <NavLink to="/login">
+              <div className="nav-item nav-button" id="register">
+                Login
+              </div>
+            </NavLink>
+                </div>
+          )}
+          {user ? (
+            <div
+              id="profile-circle"
+              onClick={() => {
+                setIsShowing((prev) => {
+                  return !prev;
+                });
+              }}
+            >
+              {user?.name[0]}
+            </div>
+          ) : (
+            ""
+          )}
+        </ul>
+        <div
+          id="prifile-dropdow"
+          style={{ display: isShowing ? "block" : "none" }}
+        >
+          <div>{user?.name}</div>
+          <hr></hr>
+          <div id="logout" onClick={logout}>
+            Logout
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NavBar;
