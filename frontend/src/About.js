e93d67c72b260c8d618fa62108037c441b4b24e0
Ola Wiebe <eeken07@gmail.com>
2021-01-25 09:46:41 +0100

    about page

---
+++ frontend/src/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About() {
+  return (
+    <div style={{ padding: "20px", maxWidth: "600px" }}>
+      This is a wake up forum for wake up people!
+    </div>
+  );
+}
+
+export default About;
