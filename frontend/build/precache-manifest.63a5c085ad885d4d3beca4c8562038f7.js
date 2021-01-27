b55aac40b1ec414dca7fef2f014e9edb26506ad7
Ola Wiebe <eeken07@gmail.com>
2021-01-27 13:52:35 +0100

    build

---
+++ frontend/build/precache-manifest.63a5c085ad885d4d3beca4c8562038f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d817494289873d20f8a25dc4aba03c8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3ca7842f8f9e61e33aa",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "d3bee0b484b96f1c1a20",
+    "url": "/static/css/main.b9e8bba4.chunk.css"
+  },
+  {
+    "revision": "d3ca7842f8f9e61e33aa",
+    "url": "/static/js/2.066f6145.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.066f6145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3bee0b484b96f1c1a20",
+    "url": "/static/js/main.46a16e12.chunk.js"
+  },
+  {
+    "revision": "607453c95d7dc95706f9",
+    "url": "/static/js/runtime-main.d0588bb2.js"
+  }
+]);
